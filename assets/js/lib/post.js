311a3e1886bb653a20a71fa267387c5c5d64e605
Tobias Quante <t.quante@outlook.com>
2021-08-18 19:41:43 +0200

    feat: h2 in posts will now scroll into view

--- assets/js/lib/post.js
+++ assets/js/lib/post.js
@@ -1,26 +1,13 @@
 /**
  * Functions specifially for posts and post related items
- * 1) Filter post list on index page DEPRECATED
- * 2) Focus the searchbar when pressing CTRL + K
- * 3) Monitor reading progress
- * 4) Toggle the post navbar when user scrolls down in the post content
- * 5) Show the post cards once user scrolls below 95% of the post
- * 6) Hide a part of the excerpt in the post card
+ * 1) Monitor reading progress
+ * 2) Toggle the post navbar when user scrolls down in the post content
+ * 3) Show the post cards once user scrolls below 95% of the post
+ * 4) Hide a part of the excerpt in the post card
+ * 5) Scroll to a clicked h2 element
  */
 
-// 2)
-const focusPostListSearchButton = (event, searchfield) => {
-  if (event.key.toLowerCase() === 'escape' && !!searchfield) {
-    event.preventDefault();
-    searchfield.blur();
-  }
-  if (event.ctrlKey && event.key.toLowerCase() === 'k' && !!searchfield) {
-    event.preventDefault();
-    searchfield.focus()
-  }
-}
-
-// 3)
+// 1)
 const monitorReadingProgress = function (contentArea, progressBar) {
 
   let frameListening = function () {
@@ -37,14 +24,13 @@ const monitorReadingProgress = function (contentArea, progressBar) {
         (progressBar.max * Math.abs(contentBox.top - midPoint)) /
         contentBox.height;
     }
-
     window.requestAnimationFrame(frameListening);
 
   };
   window.requestAnimationFrame(frameListening);
 };
 
-// 4)
+// 2)
 const monitorPostNavbar = (domPostReadingProgressBar, duration) => {
   const readingProgress = domPostReadingProgressBar.value
   if (readingProgress > 0.05) {
@@ -56,10 +42,10 @@ const monitorPostNavbar = (domPostReadingProgressBar, duration) => {
   }
 }
 
-// 5)
+// 3)
 const monitorShowReadMorePostCards = (domPostReadingProgressBar) => {
   const readingProgress = domPostReadingProgressBar.value
-  if(readingProgress < 0.98) {
+  if (readingProgress < 0.98) {
     animateSlideOutItems(500, 'bottom', '.q-post-card-wrapper')
   }
 
@@ -68,12 +54,20 @@ const monitorShowReadMorePostCards = (domPostReadingProgressBar) => {
   }
 }
 
-// 6)
+// 4)
 const hidePostCardExcerpts = (domPostExcerptItems) => {
   domPostExcerptItems.forEach(excerptItem => {
     const newText = excerptItem.innerText.split(' ').splice(0, 20);
     excerptItem.innerText = [...newText, '...'].join(' ');
   })
 }
+
+// 5)
+document.querySelectorAll('.q-post-article h2').forEach(element => {
+  element.addEventListener('click', () => {
+    const coord = element.getBoundingClientRect().top + window.scrollY - 50;
+    window.scroll({ top: coord })
+  })
+})
 
 hidePostNavbar(0)
